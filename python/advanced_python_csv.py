282e0f486f32f30d358f321b02e94f75d4c54de0
patricknieto <patrickrnieto@gmail.com>
2016-03-28 18:55:45 -0500

    updates to advanced python csv

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,8 @@
-PLACE YOUR CODE HERE
+
+
+#Q5. Write email addresses from Part I to csv file
+
+with open('emails.csv', 'w') as f:
+    for email in data.email.values:
+        f.write('%s\n' % email)
+
