ed75edc3a9539d77b80a5b62569abfd47e1b564d
patricknieto <patrickrnieto@gmail.com>
2016-03-29 15:36:23 -0500

    updates to csv advanced python

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -2,7 +2,7 @@
 
 #Q5. Write email addresses from Part I to csv file
 
-with open('emails.csv', 'w') as f:
+with open('emails.csv', 'w') as fp:
     for email in data.email.values:
-        f.write('%s\n' % email)
+        fp.write('%s\n' % email)
 
